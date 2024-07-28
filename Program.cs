242ec6affc46c98dc577649e4c621f5104fbdc68
abhijittupe <136335995+abhijittupe@users.noreply.github.com>
2024-07-28 22:47:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using DbFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<YcpJuly23Context>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("YcpJuly23Context")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Employees}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
